a2b6d4832094a970706f759ee28cc9849d16bad6
Rafael Pereira <rafaelszp.info@gmail.com>
2017-04-11 16:20:08 -0300

    Update work with gitlab

---
+++ src/main/java/szp/rafael/example/xampllib/Main.java
@@ -0,0 +1,18 @@
+package szp.rafael.example.xampllib;
+
+/**
+ * Created by rafael on 4/10/17.
+ */
+public class Main {
+
+  public static void main(String... args){
+	Main main = new Main();
+	main.tada();
+  }
+
+  public void tada() {
+	System.out.println("TADA!!");
+  }
+
+
+}
---
+++ src/test/java/unit/szp/rafael/example/xampllib/MainTest.java
@@ -0,0 +1,17 @@
+package unit.szp.rafael.example.xampllib;
+
+import org.testng.annotations.Test;
+import szp.rafael.example.xampllib.Main;
+
+/**
+ * Created by rafael on 4/10/17.
+ */
+public class MainTest {
+
+  @Test
+  public void shouldTada(){
+  	Main m = new Main();
+  	m.tada();
+  }
+
+}
